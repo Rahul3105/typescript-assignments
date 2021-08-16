5e7f0f0616d3d667b0a7fdfda034d9cdf5d1ddb0
rahul rajput <rahulrajput7358@gmail.com>
2021-08-16 17:59:57 +0530

    typescript-assignment-1-added

---
+++ typescript1/src/index.js
@@ -0,0 +1,8 @@
+var a;
+a = 1;
+console.log(a);
+var b = [2, 2, 3];
+b.push(10);
+console.log(b);
+var _a = [1, 2], r = _a[0], c = _a[1];
+console.log([r, c]);
---
+++ typescript1/src/index.ts
@@ -0,0 +1,10 @@
+let a: number;
+a = 1;
+console.log(a)
+
+
+let b: number[] = [2, 2, 3];
+b.push(10);
+console.log(b)
+let [r, c] = [1, 2]
+console.log([r, c])
